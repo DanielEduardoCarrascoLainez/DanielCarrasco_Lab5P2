4b3f9ba3300aff31720225b1c07360b7ae5445ed
DanielEduardoCarrascoLainez <dcarrasco04@unitec.edu>
2023-02-24 14:09:55 -0600

    Clase Personajes

--- src/Heroes/Personajes.java
+++ src/Heroes/Personajes.java
@@ -5,5 +5,87 @@
 package Heroes;
 
 public class Personajes {
+    private String nombre;
+    private String poder;
+    private String debilidad;
+    private int fuerza;
+    private int agilidad;
+    private int agilidad_M;
+    private int HP;
+
+    public Personajes() {
+    }
+
+    public Personajes(String nombre, String poder, String debilidad, int fuerza, int agilidad, int agilidad_M, int HP) {
+        this.nombre = nombre;
+        this.poder = poder;
+        this.debilidad = debilidad;
+        this.fuerza = fuerza;
+        this.agilidad = agilidad;
+        this.agilidad_M = agilidad_M;
+        this.HP = HP;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPoder() {
+        return poder;
+    }
+
+    public void setPoder(String poder) {
+        this.poder = poder;
+    }
+
+    public String getDebilidad() {
+        return debilidad;
+    }
+
+    public void setDebilidad(String debilidad) {
+        this.debilidad = debilidad;
+    }
+
+    public int getFuerza() {
+        return fuerza;
+    }
+
+    public void setFuerza(int fuerza) {
+        this.fuerza = fuerza;
+    }
+
+    public int getAgilidad() {
+        return agilidad;
+    }
+
+    public void setAgilidad(int agilidad) {
+        this.agilidad = agilidad;
+    }
+
+    public int getAgilidad_M() {
+        return agilidad_M;
+    }
+
+    public void setAgilidad_M(int agilidad_M) {
+        this.agilidad_M = agilidad_M;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    @Override
+    public String toString() {
+        return "Personajes{" + "nombre=" + nombre + ", poder=" + poder + ", debilidad=" + debilidad + ", fuerza=" + fuerza + ", agilidad=" + agilidad + ", agilidad_M=" + agilidad_M + ", HP=" + HP + '}';
+    }
+    
     
 }
